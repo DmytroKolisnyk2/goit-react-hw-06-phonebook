97a4c45ed4bb36306712549e5cc4c1fe80751373
DmytroKolisnyk2 <d.s.kolisnyk@i.ua>
2022-01-26 15:31:52 +0200

    fix bug

--- src/redux/contacts/contacts-reducers.js
+++ src/redux/contacts/contacts-reducers.js
@@ -1,6 +1,6 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import { items } from "./items/items-reducers";
-import { filter } from "./contacts/filter/filter-reducers";
+import { filter } from "./filter/filter-reducers";
 
 export const contacts = combineReducers({
   items,
